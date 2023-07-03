ab9ead6847c35976b7fbe76d94cd6e13f98050ec
J M <montytyper@msn.com>
2023-07-02 20:24:32 -0700

    Added Input handling
    The way this works is that all inputs are routed into a global input
    handler, which immediately passes them off to the Menu object base class.
    If a menu overrides the appropriate handler, they can receive key events
    whenever they're active.

--- main.lua
+++ main.lua
@@ -17,12 +17,37 @@ spritelib = gfx.sprite
 screenWidth = playdate.display.getWidth()
 screenHeight = playdate.display.getHeight()
 
-local gameState = {initial, playing, paused, over}
+local gameState = {}
 local kGameInitialState, kGamePlayingState, kGamePauseState, kGameOverState = 1, 2, 3, 4
 local currentGameState = kGameInitialState
 
 
 local screens = {CounterScreen(), PlayScreen(), PauseScreen(), GameOverScreen()}
+
+local inputHandlers = {
+    AButtonDown = function() screens[currentGameState]:ButtonHandler_A(false, false) end,
+    AButtonHeld = function() screens[currentGameState]:ButtonHandler_A(false, true) end,
+    AButtonUp = function() screens[currentGameState]:ButtonHandler_A(true, false) end,
+
+    BButtonDown = function() screens[currentGameState]:ButtonHandler_B(false, false) end,
+    BButtonHeld = function() screens[currentGameState]:ButtonHandler_B(false, true) end,
+    BButtonUp = function() screens[currentGameState]:ButtonHandler_B(true, false) end,
+
+    downButtonDown = function() screens[currentGameState]:ButtonHandler_DPad(DPadDown, false) end,
+    downButtonUp = function() screens[currentGameState]:ButtonHandler_DPad(DPadDown, true) end,
+
+    leftButtonDown = function() screens[currentGameState]:ButtonHandler_DPad(DPadLeft, false) end,
+    leftButtonUp = function() screens[currentGameState]:ButtonHandler_DPad(DPadLeft, true) end,
+
+    rightButtonDown = function() screens[currentGameState]:ButtonHandler_DPad(DPadRight, false) end,
+    rightButtonUp = function() screens[currentGameState]:ButtonHandler_DPad(DPadRight, true) end,
+
+    upButtonDown = function() screens[currentGameState]:ButtonHandler_DPad(DPadUp, false) end,
+    upButtonUp = function() screens[currentGameState]:ButtonHandler_DPad(DPadUp, true) end,
+
+    cranked = function(change, acc) screens[currentGameState]:CrankHandler(change, acc) end,
+
+}
 
 local function toGameMode(mode)
     if mode >= kGameInitialState and mode <= kGameOverState then
@@ -51,13 +76,17 @@ local function initialize()
     for i, screen in ipairs(screens) do
         screen:Initialize()
     end
+
+    playdate.inputHandlers.push(inputHandlers)
 end
 
 function playdate.gameWillTerminate()
+    playdate.inputHandlers.pop()
     playdate.datastore.write(save, "gamestate")
 end
 
 function playdate.deviceWillSleep()
+    playdate.inputHandlers.pop()
     playdate.datastore.write(save, "gamestate")
 end
 
--- sources/Menu.lua
+++ sources/Menu.lua
@@ -1,7 +1,8 @@
 import "CoreLibs/object"
 
-
 class('Menu').extends()
+
+DPadUp, DPadDown, DPadLeft, DPadRight = 1, 2, 3, 4
 
 function Menu:Initialize()
     return true
@@ -14,4 +15,25 @@ end
 function Menu:UpdateScreen()
 
     return true
+end
+
+
+function Menu:ButtonHandler_A(isPressed, held)
+    output = string.format("A Pressed: %s, Held: %s", tostring(isPressed), tostring(held))
+    print(output)
+end
+
+function Menu:ButtonHandler_B(isPressed, held)
+    output = string.format("B Pressed: %s, Held: %s", tostring(isPressed), tostring(held))
+    print(output)
+end
+
+function Menu:ButtonHandler_DPad(button, isPressed)
+    output = string.format("DPad Button %s Pressed: %s", tostring(button), tostring(isPressed))
+    print(output)
+end
+
+function Menu:CrankHandler(change, acceleration)
+    output = string.format("Crank change: %s, acc: %s", tostring(change), tostring(acceleration))
+    print(output)
 end
